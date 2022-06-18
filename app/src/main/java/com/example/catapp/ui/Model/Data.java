f4cf9d4762951f36633ac75254cf201dbeec765a
usantiago1 <usantiago0309@gmail.com>
2022-06-17 22:31:11 -0500

    gg

--- app/src/main/java/com/example/catapp/ui/Model/Data.java
+++ app/src/main/java/com/example/catapp/ui/Model/Data.java
@@ -7,15 +7,19 @@ public class Data {
      public String getFact(){
          return fact;
      }
+
      public String getLength(){
          return length;
      }
+
      public void setFact(String fact){
          this.fact = fact;
      }
+
      public void setLength(String length){
          this.length = length;
      }
+
      public void DataConstructor(String fact, String length){
          this.fact = fact;
          this.length = length;
